44dd42af159098f165ee02cb3aebc2d1e4befc65
junyiz <junyime@qq.com>
2017-05-12 14:36:42 +0800

    use zenio.js

--- README.md
+++ README.md
@@ -10,18 +10,18 @@ npm install koa-http-request --save
 # Usage
 
 ```javascript
-var koa = require('koa');
-var koaRequest = require('koa-http-request');
-var app = new koa();
+const koa = require('koa');
+const koaRequest = require('koa-http-request');
+const app = new koa();
 
 app.use(koaRequest({
-  dataType: 'json', //automatically parsing of JSON response
+  json: true, //automatically parsing of JSON response
   timeout: 3000,    //3s timeout
   host: 'https://api.github.com'
 }));
 
 app.use(async (ctx, next) => {
-    var repo = await ctx.get('/repos/junyiz/koa-http-request', null, {
+    let repo = await ctx.get('/repos/junyiz/koa-http-request', null, {
         'User-Agent': 'koa-http-request'
     });
     ctx.body = 'repos id: ' + repo.id + '\nrepos name: ' + repo.full_name;
@@ -32,10 +32,10 @@ app.listen(process.env.PORT || 8090);
 
 # Options
 
-##### dataType
+##### json
 
-Type: 'String'
-Default: ''
+Type: 'Boolean'
+Default: false
 
 Parse response body with `JSON.parse` and set `accept` header to `application/json`.
 
@@ -54,11 +54,11 @@ Default: ''
 
 # API
 
-#### ctx.get(url, [params], [headers])
-#### ctx.head(url, [params], [headers])
-#### ctx.post(url, [params], [headers])
-#### ctx.put(url, [params], [headers])
-#### ctx.delete(url, [params], [headers])
+#### ctx.get(url[, params[, headers]])
+#### ctx.head(url[, params[, headers]])
+#### ctx.post(url[, params[, headers]])
+#### ctx.put(url[, params[, headers]])
+#### ctx.delete(url[, params[, headers]])
 
 # License
 MIT
--- index.js
+++ index.js
@@ -1,136 +1,16 @@
-var http = require('http');
-var https = require('https');
-var urlParse = require('url').parse;
-var qsStringify = require('querystring').stringify;
-var iconv = require('iconv-lite');
-var BufferHelper = require('bufferhelper');
-var options = {
-    host: '',
-    dataType: '',
-    timeout: 30000
-};
+const zenio = require('zenio')
 
-/**
- *  @param {Object} params
- */
-function createParams(params) {
-    var ret = [];
-
-    for (var i in params) {
-        ret.push(i + '=' + encodeURIComponent(params[i]));
-    }
-
-    return ret.join('&');
-}
-
-/**
- *  @param {String} url
- *  @param {String} method  GET | HEAD | POST | PUT | PATCH | DELETE
- *  @param {Object} headers
- */
-function createOptions(url, method, headers) {
-    url = urlParse(url);
-
-    return {
-        hostname: url.hostname,
-        port: url.protocol == 'https:' ? url.port || 443 : url.port || 80,
-        path: url.path,
-        method: method,
-        headers: Object.assign({
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }, headers)
-    };
-}
-
-/**
- *  @param {String} method  GET | HEAD | POST | PUT | PATCH | DELETE
- */
-function request(method) {
-    return function (url, params, headers) {
-        var cb, result, req, abort, opts, httpReqeust;
-
-        url = /^http/.test(url) ? url : options.host + url;
-        httpReqeust = /^https/.test(url) ? https.request : http.request;
-
-        function done() {
-            if (options.dataType == 'json') {
-                try {
-                    result = JSON.parse(result);
-                } catch(e) {
-                    result = e;
-                }
-            }
-            cb(result);
-        }
-
-        function error(e) {
-            clearTimeout(abort);
-            cb(JSON.stringify(e));
-        }
-
-        if ((method == 'GET' || method == 'HEAD') && typeof params === 'object' && params !== null) {
-            url += (url.indexOf('?') > -1 ? '&' : '?') + createParams(params);
-        }
-
-        opts = createOptions(url, method, headers);
-        
-        if (options.dataType == 'json' && opts.headers.accept === undefined) {
-            opts.headers.accept = 'application/json';
-        }
-
-        if (method == 'POST' || method == 'PUT' || method == 'PATCH') {
-            if (typeof params === 'object' && params !== null) {
-                params = qsStringify(params);
-                opts.headers['Content-Length'] = params.length;
-            } else {
-                opts.headers['Content-Length'] = 0;
-            }
-        }
-
-        abort = setTimeout(function() {
-            req.abort();
-            error(url + ' timeout');
-        }, options.timeout);
-
-        req = httpReqeust(opts, function(res) {
-            var bufferHelper = new BufferHelper();
-
-            clearTimeout(abort);
-            res.on('data', function(chunk) {
-                bufferHelper.concat(chunk);
-            }).on('end', function() {
-                result = iconv.decode(bufferHelper.toBuffer(), 'utf8');
-
-                if (res.statusCode === 200) {
-                    done();
-                } else {
-                    error(url + ' : statusCode=' + res.statusCode + '\n' + result);
-                }
-            });
-        });
-
-        req.on('error', error);
-
-        if ((method == 'POST' || method == 'PUT' || method == 'PATCH') && params) {
-            req.write(params);
-        }
-
-        req.end();
-
-        return new Promise(function(resolve) {
-            cb = resolve;
-        });
-    }
-}
-
-module.exports = function(opts) {
-    options = Object.assign(options, opts || {});
-
-    return async function(ctx, next) {
-        ['get', 'head', 'post', 'put', 'patch', 'delete'].forEach(function(item) {
-            ctx[item] = request(item.toUpperCase())
-        })
-
-        await next();
-    }
+module.exports = function (opts) {
+  opts = opts || {}
+  
+  if (opts.dataType == 'json') opts.json = true
+  
+  zenio.setOptions(opts)
+  
+  return async (ctx, next) => {
+    zenio.methods.forEach(method => {
+      ctx[method] = zenio[method]
+    })
+    await next()
+  }
 }
